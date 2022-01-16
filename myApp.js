999ca750e0dc42706ea7392d143c516c9ae66008
errolcostabir <errolcostabir98@gmail.com>
2022-01-16 16:54:53 +0530

    Added express.static

--- myApp.js
+++ myApp.js
@@ -8,6 +8,6 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+'/views/index.html');
 });
     
-app.use(express.static(__dirname+'/public'));
+app.use('/public',express.static(__dirname+'/public'));
 
 module.exports=app;
