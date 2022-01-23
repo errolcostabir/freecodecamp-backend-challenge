6b07f6b431b48b46f41aa39312c9a5c77826ab25
errolcostabir <errolcostabir98@gmail.com>
2022-01-23 17:27:41 +0530

    added middleware

--- myApp.js
+++ myApp.js
@@ -4,12 +4,19 @@ require('dotenv').config();
 
 //console.log("Hello World");
 
+app.get('/json',function(req,res){
+    console.log(req.method+" "+req.path+" - "+req.ip);
+    next();
+});
+
 app.get('/',function(req,res){
     //res.send("Hello Express");
     res.sendFile(__dirname+'/views/index.html');
+    console.log(req.method+" "+req.path+" - "+req.ip);
+    next();
 });
 
-app.get('/json',function(req,res){
+/* app.get('/json',function(req,res){
     //res.json({"message": "Hello json"});
     //console.log(process.env.MESSAGE_STYLE);
     //var mStyle=process.env.MESSAGE_STYLE;
@@ -19,7 +26,7 @@ app.get('/json',function(req,res){
     }
     
     res.json({"message": message});
-});
+}); */
     
 app.use('/public',express.static(__dirname+'/public'));
 
